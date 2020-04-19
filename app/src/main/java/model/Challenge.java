5bf593f92a8a74065b997854785f6c03940b5843
Gaëlle Ferreira <gaelle_f@live.fr>
2020-04-19 16:08:44 +0200

    changement de noms de variables constructeur

--- app/src/main/java/model/Challenge.java
+++ app/src/main/java/model/Challenge.java
@@ -8,43 +8,35 @@ public class Challenge {
     private String type_challenge;
     private int xp_challenge;
 
-    public Challenge(int id, String n, String d, String t, int xp){
-
+    public Challenge(int id, String name, String description, String type, int xp){
          this.id_challenge = id;
-         this.description_challenge = d;
-         this.name_challenge = n;
-         this.type_challenge = t;
+         this.description_challenge = description;
+         this.name_challenge = name;
+         this.type_challenge = type;
          this.xp_challenge = xp;
-
     }
 
     public int getId_challenge() {
-
         return this.id_challenge;
     }
 
     public String getName_challenge(){
-
         return this.name_challenge;
     }
 
     public String getDescription_challenge(){
-
         return this.description_challenge;
     }
 
     public String getType_challenge(){
-
         return this.type_challenge;
     }
 
     public int getXp_challenge(){
-
         return this.xp_challenge;
     }
 
     public String toString(){
-
         String s = "DetailChallenge numéro " + this.id_challenge + " " + this.name_challenge + "catégorie: " + this.type_challenge + " Objectif: " + this.description_challenge + " " + this.xp_challenge + " XP";
         return s;
     }
