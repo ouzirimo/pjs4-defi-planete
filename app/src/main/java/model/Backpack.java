87ea4491870f0a1b84f951e42228afc4c6b70466
Gaëlle Ferreira <gaelle_f@live.fr>
2020-04-19 17:29:50 +0200

    new methods in User class to link backpack

--- app/src/main/java/model/Backpack.java
+++ app/src/main/java/model/Backpack.java
@@ -1,4 +1,26 @@
 package model;
 
+/**
+ * Backpack class link challenge object to User object
+ * @author Gaëlle & Noëlle
+ * @version 1.0
+ */
 public class Backpack {
+    private Challenge challenge;
+    private boolean done;
+
+    public Backpack(Challenge challenge) {
+        this.challenge = challenge;
+    }
+
+    public void setDone(boolean done){
+        this.done=done;
+    }
+    public boolean isDone() {
+        return done;
+    }
+    public Challenge getChallenge() {
+        return challenge;
+    }
+
 }
